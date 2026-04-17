b6168abd80021fd70cbcacd28499d817e15d1632
rian18-ari <fikririan16@gmail.com>
2026-04-17 21:23:29 +0700

    nambha field phone sama image di table user

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('phone')->unique();
+            $table->string('image')->nullable();
             $table->enum('role', ['owner', 'user', 'admin'])->default('user');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
